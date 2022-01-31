d039da637242a733b203c61cffc416bb7c7a2c55
Axel Aquino <aquino.axel@gmail.com>
2022-01-31 17:13:35 -0500

    commit tree e88bbc00d300aae69ebab4ea97a7e0f022f49e17

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(event = 'roller-skate') {
+  return `This Saturday, I want to ${event}!`;  
+}
+
+const mondayWork = function (event = 'go to the office'){
+    return `This Monday, I will ${event}.`;
+};
+
+
+function wrapAdjective(string = '*') {
+    return function (adj = 'special') {
+        return `You are ${string}${adj}${string}!`
+    }
+} 
+
+const encouragingPromptFunction = wrapAdjective("*")("a hard worker");
